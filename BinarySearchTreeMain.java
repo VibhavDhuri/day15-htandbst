a5c75d1c3b2286edb17b4e5bd2f54b73e91dd319
VibhavDhuri <vibhavpdhuri@gmail.com>
2022-07-28 03:20:18 +0530

    Added: uc1 for binary search tree

---
+++ BinarySearchTreeMain.java
@@ -0,0 +1,16 @@
+
+public class BinarySearchTreeMain {
+
+	public static void main(String[] args) {
+		BinarySearchTree binaryTree = new BinarySearchTree();
+
+		Node node = new Node(56); // head node
+		binaryTree.root = node;
+		binaryTree.insertElement(node, 30);
+		binaryTree.insertElement(node, 70);
+
+		System.out.println("Display Node");
+		binaryTree.traverseTree(node);
+	}
+
+}
